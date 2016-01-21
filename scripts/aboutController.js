d2f53c3da9205ace10700f8e94e43b9e91af712e
Sawako Ishida <sawako.yoriko@gmail.com>
2016-01-21 08:43:19 -0800

    added Procfile and edited package.json file

---
+++ scripts/aboutController.js
@@ -0,0 +1,10 @@
+(function(module){
+  var aboutController = {};
+  checkUpdate();
+  aboutController.about = function(){
+    $('body section').hide();
+    $('#edu').show();
+    repoData.grabRepo(repoView.index());
+  }
+  window.aboutController = aboutController;
+})(window);
---
+++ scripts/routes.js
@@ -0,0 +1,6 @@
+// page.base('');
+page('/', indexController.index);
+page('/about', aboutController.about);
+// page('*', notFound);
+
+page();
